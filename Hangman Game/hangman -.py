c12755647d12e037d1996baf827ea4fd90c62d08
Kadir Erdem Kıran <erdem56hj56@gmail.com>
2025-11-12 10:42:48 +0300

    Adam-Asmaca
    Python ile oluşturulmuş, klasik adam asmaca oyunudur.

---
+++ Hangman Game/hangman -.py
@@ -0,0 +1,67 @@
+import random
+from tokenize import cookie_re
+
+import hangman_words
+import hangman_art
+
+
+
+
+
+lives = 6
+
+
+print(hangman_art.logo)
+
+
+
+chosen_word = random.choice(hangman_words.word_list)
+print(chosen_word)
+
+placeholder = ""
+word_length = len(chosen_word)
+for position in range(word_length):
+    placeholder += "_"
+print("Word to guess: " + placeholder)
+
+game_over = False
+correct_letters = []
+
+while not game_over:
+
+    print("****************************<???>/6 LIVES LEFT****************************")
+    guess = input("Guess a letter: ").lower()
+
+
+    if guess in correct_letters:
+        print("You've already guessed this.")
+
+
+    display = ""
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(guess)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+
+    print("Word to guess: " + display)
+
+
+    if guess not in chosen_word:
+        lives -= 1
+
+        if lives == 0:
+            game_over = True
+
+            print(f"***********************YOU LOSE**********************")
+            print(f"The word = {chosen_word}")
+
+    if "_" not in display:
+        game_over = True
+        print("****************************YOU WIN****************************")
+
+    print(hangman_art.stages[lives])
